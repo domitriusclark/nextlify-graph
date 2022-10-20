4ae8f56d92efd0397c9f2205a9e5e243a8910040
Domitrius Clark <domitriusaclark@gmail.com>
2022-10-20 15:45:11 -0400

    Fixed compiler path issues

--- pages/subscription.tsx
+++ pages/subscription.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useQuery } from "@tanstack/react-query";
 import type { NextPage } from "next";
-import NetlifyGraph from "../lib/netlifyGraph";
+import NetlifyGraph from "@lib/netlifyGraph";
 
 const SubscriptionPage: NextPage = () => {
   return (
